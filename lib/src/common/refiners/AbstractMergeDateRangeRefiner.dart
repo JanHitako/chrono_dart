024e6cf7dc2d66127f58d6e05ef84d2f3108b9da
JanHitako <narhhj@gmail.com>
2025-03-10 16:59:34 +0300

    fixing day parsing bugs from differences in Day.js and Day.dart

--- lib/src/common/refiners/AbstractMergeDateRangeRefiner.dart
+++ lib/src/common/refiners/AbstractMergeDateRangeRefiner.dart
@@ -1,18 +1,10 @@
 import 'dart:math' show min;
+
 import '../../results.dart' show ParsingResult;
 import '../../types.dart' show Component;
 import '../abstract_refiners.dart' show MergingRefiner;
 
 abstract class AbstractMergeDateRangeRefiner extends MergingRefiner {
-  RegExp patternBetween();
-
-  @override
-  bool shouldMergeResults(textBetween, currentResult, nextResult, context) {
-    return currentResult.end == null &&
-        nextResult.end == null &&
-        patternBetween().hasMatch(textBetween);
-  }
-
   @override
   // ignore: avoid_renaming_method_parameters
   ParsingResult mergeResults(textBetween, fromResult, toResult, context) {
@@ -36,24 +28,24 @@ abstract class AbstractMergeDateRangeRefiner extends MergingRefiner {
       var fromMoment = fromResult.start.dayjs();
       var toMoment = toResult.start.dayjs();
       if (toResult.start.isOnlyWeekdayComponent() &&
-          toMoment.add(7, "days")!.isAfter(fromMoment)) {
-        toMoment = toMoment.add(7, "days")!;
+          toMoment.add(7, "d")!.isAfter(fromMoment)) {
+        toMoment = toMoment.add(7, "d")!;
         toResult.start.imply(Component.day, toMoment.date());
         toResult.start.imply(Component.month, toMoment.month());
         toResult.start.imply(Component.year, toMoment.year());
       } else if (fromResult.start.isOnlyWeekdayComponent() &&
-          fromMoment.add(-7, "days")!.isBefore(toMoment)) {
-        fromMoment = fromMoment.add(-7, "days")!;
+          fromMoment.add(-7, "d")!.isBefore(toMoment)) {
+        fromMoment = fromMoment.add(-7, "d")!;
         fromResult.start.imply(Component.day, fromMoment.date());
         fromResult.start.imply(Component.month, fromMoment.month());
         fromResult.start.imply(Component.year, fromMoment.year());
       } else if (toResult.start.isDateWithUnknownYear() &&
-          toMoment.add(1, "years")!.isAfter(fromMoment)) {
-        toMoment = toMoment.add(1, "years")!;
+          toMoment.add(1, "y")!.isAfter(fromMoment)) {
+        toMoment = toMoment.add(1, "y")!;
         toResult.start.imply(Component.year, toMoment.year());
       } else if (fromResult.start.isDateWithUnknownYear() &&
-          fromMoment.add(-1, "years")!.isBefore(toMoment)) {
-        fromMoment = fromMoment.add(-1, "years")!;
+          fromMoment.add(-1, "y")!.isBefore(toMoment)) {
+        fromMoment = fromMoment.add(-1, "y")!;
         fromResult.start.imply(Component.year, fromMoment.year());
       } else {
         [toResult, fromResult] = [fromResult, toResult];
@@ -71,5 +63,14 @@ abstract class AbstractMergeDateRangeRefiner extends MergingRefiner {
     }
 
     return result;
+  }
+
+  RegExp patternBetween();
+
+  @override
+  bool shouldMergeResults(textBetween, currentResult, nextResult, context) {
+    return currentResult.end == null &&
+        nextResult.end == null &&
+        patternBetween().hasMatch(textBetween);
   }
 }
